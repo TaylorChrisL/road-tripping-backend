7bb3af62ee6982cc1aff351ae8a8d5143baa7c9a
Chris Taylor <ctaylor534@gmail.com>
2022-07-29 12:46:22 -0700

    Adds routes for trips and places, defaults the format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  post "/users" => "users#create"
-  post "/sessions" => "sessions#create"
+  post "/users" => "users#create", default: { format: :json }
+  post "/sessions" => "sessions#create", default: { format: :json }
+
+  resources :trips, :places, defaults: { format: :json }
 end
